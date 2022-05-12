27245db4cd7f4421f3157e3a242a139b57e1beca
RyanChristopher23 <104362877+RyanChristopher23@users.noreply.github.com>
2022-05-12 16:02:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function one(){
+    window.alert("Vincentian priest Pedro Maria Boss first suggested placing a Christian monument on Mount Corcovado in the mid-1850s to honor Princess Isabel, regent of Brazil and the daughter of Emperor Pedro II, but the project was not approved.[1] In 1889 the country became a republic, and owing to the separation of church and state the proposed statue was dismissed.")
+}
+function two(){
+    window.alert("The Taj Mahal was designated as a UNESCO World Heritage Site in 1983 for beingthe jewel of Muslim art in India and one of the universally admired masterpieces of the worlds heritage. It is regarded by many as the best example of Mughal architecture and a symbol of India's rich history. The Taj Mahal attracts more than 6 million visitors a year[3] and in 2007, it was declared a winner of the New 7 Wonders of the World (2000–2007) initiative.")
+}
+function three(){
+    window.alert("The frontier walls built by different dynasties have multiple courses. Collectively, they stretch from Liaodong in the east to Lop Lake in the west, from the present-day Sino–Russian border in the north to Tao River (Taohe) in the south; along an arc that roughly delineates the edge of the Mongolian steppe; spanning 21,196.18 km (13,170.70 mi) in total.[7][3] Today, the defensive system of the Great Wall is generally recognized as one of the most impressive architectural feats in history.")
+}
+function four(){
+    window.alert("The pyramid consists of a series of square terraces with stairways up each of the four sides to the temple on top. Sculptures of plumed serpents run down the sides of the northern balustrade. Around the spring and autumn equinoxes, the late afternoon sun strikes off the northwest corner of the pyramid and casts a series of triangular shadows against the northwest balustrade, creating the illusion of the feathered serpent crawling down the pyramid. To contemporary visitors, the event has been very popular and is witnessed by thousands at the spring equinox, but it is not known whether the phenomenon is a result of a purposeful design since the light-and-shadow effect can be observed without major changes during several weeks near the equinoxes")
+}
+function five(){
+    window alert("The trading business gained the Nabataeans considerable revenue and Petra became the focus of their wealth. The Nabataeans were accustomed to living in the barren deserts, unlike their enemies, and were able to repel attacks by taking advantage of the area's mountainous terrain. They were particularly skillful in harvesting rainwater, agriculture and stone carving. Petra flourished in the 1st century AD, when its famous Al-Khazneh structure – believed to be the mausoleum of Nabataean king Aretas IV – was constructed, and its population peaked at an estimated 20,000 inhabitants.")
+}
+function six(){
+    window alert("The Incas, in contrast with the Mayans, had no written language, and no European visited the site until the 19th century, so far as is known. There are, therefore, no written records of the site while it was in use. The names of the buildings, their supposed uses, and their inhabitants are all the product of modern archeologists, on the basis of physical evidence, including tombs at the site.")
+}
+function seven(){
+    window alert("The Colosseum is built of travertine limestone, tuff (volcanic rock), and brick-faced concrete. It could hold an estimated 50,000 to 80,000 spectators at various points in its history,[4][5] having an average audience of some 65,000;[6] it was used for gladiatorial contests and public spectacles including animal hunts, executions, re-enactments of famous battles, and dramas based on Roman mythology, and briefly mock sea battles. The building ceased to be used for entertainment in the early medieval era. It was later reused for such purposes as housing, workshops, quarters for a religious order, a fortress, a quarry, and a Christian shrine.")
+}
